b11d678134ef3fafecc015c0a7821af51b909b07
Lazaro Pereira <spmlaz@gmail.com>
2017-11-15 06:36:05 -0800

    first commit

--- netsense.py
+++ netsense.py
@@ -5,7 +5,8 @@ import pprint
 import requests
 import json
 
-
+server = 'ns-us-dcc.sensity.com'
+man-server='man-leg.sensity.com'
 
 def results():
     # specify the url
@@ -19,26 +20,23 @@ def results():
     print(json.loads(url.text)[0]['name'])
     mfgorgid = json.loads(url.text)[0]['orgid']
 
-    sitelist = session.get(url='https://' + servername.servername + '/v3.0/customers/' + mfgorgid + '/sites' , headers=headers , verify=False)
+    sitelist = session.get(url='https://' + servername.servername + '/v3.0/customers/' + mfgorgid + '/sites' , headers=headers )
 
     responsedict = {}
+
     for i in json.loads(sitelist.text):
         responsedict[i['name']] = i['siteid']
-
+    #pprint.pprint(responsedict)
     nodedict = {}
     nodelist = []
     for key, val in responsedict.items():
 
         nodes = session.get(url='https://' + servername.servername + '/v3.0/customers/' + mfgorgid + '/sites/'
-                                + val + '/nodes' ,)
-
+                                + val + '/nodes' )
+        url ='https://' + servername.servername + '/v3.0/customers/' + mfgorgid + '/sites/' + val + '/nodes'
         for i in json.loads(nodes.text):
-            nodelist.append(key + ':' + i['nodeid'])
+            nodelist.append(key + ':' + i['nodeid'] + ',' + url + '/' + i['nodeid'])
+    return nodelist
 
 
-    pprint.pprint(nodelist)
-
-
-
-
-results()
+pprint.pprint(results())
